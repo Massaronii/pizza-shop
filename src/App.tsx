a7d4160363a65cc6e60fe649e9df408a13c806ed
yago <yagosonicmassaroni@gmail.com>
2024-05-28 18:50:31 -0300

    config

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-
+import { Button } from "./components/ui/button";
+import "./global.css";
 
 export function App() {
- return (
-    <></>
-  )
+  return (
+    <div>
+      <Button>Enviar</Button>
+    </div>
+  );
 }
-
