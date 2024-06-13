0947651c8a6142d1df290cb020c0e97d7fc3e750
yago <yagosonicmassaroni@gmail.com>
2024-06-13 10:21:04 -0300

    :lipstick: pagina de cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
+import {Toaster} from "sonner"
 
 import "./global.css";
 import { HelmetProvider, Helmet } from "react-helmet-async";
@@ -7,8 +8,9 @@ import { HelmetProvider, Helmet } from "react-helmet-async";
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop"/>
-      <RouterProvider router={router}/>
+      <Helmet titleTemplate="%s | pizza.shop" />
+      <Toaster richColors/>
+      <RouterProvider router={router} />
     </HelmetProvider>
   );
 }
