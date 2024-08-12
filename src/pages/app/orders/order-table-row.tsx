0ad2ad43d0bd36e76399a3f893ffd704fa5e49b7
yago <yagosonicmassaroni@gmail.com>
2024-08-12 19:19:59 -0300

    feat: paginação dos pedidos

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,45 +1,66 @@
-import { Button } from "@/components/ui/button"
-import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
-import {  TableCell, TableRow } from "@/components/ui/table"
-import { ArrowRight, Search, X } from "lucide-react"
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+import { TableCell, TableRow } from "@/components/ui/table";
+import { ArrowRight, Search, X } from "lucide-react";
 import { OrderDetails } from "./order-details";
+import { OrderStatus } from "@/components/order-status";
+import {formatDistanceToNow} from "date-fns"
+import {ptBR} from "date-fns/locale"
 
-export function OrderTableRow(){
+export interface OrderTableRowProps {
+  order: {
+    orderId: string;
+    createdAt: string;
+    status: "pending" | "canceled" | "processing" | "delivering" | "delivered";
+    customerName: string;
+    total: number;
+  };
+}
+
+export function OrderTableRow({ order }: OrderTableRowProps) {
   return (
-    <>
-      <TableRow>
-        <TableCell>
-          <Dialog>
-            <DialogTrigger asChild>
-              <Button variant="outline" size="xs">
-                <Search className="h-3 w-3" />
-                <span className="sr-only">Detalhes do pedido</span>
-              </Button>
-            </DialogTrigger>
+    <TableRow>
+      <TableCell>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline" size="xs">
+              <Search className="h-3 w-3" />
+              <span className="sr-only">Detalhes do pedido</span>
+            </Button>
+          </DialogTrigger>
 
-            <DialogContent><OrderDetails/></DialogContent>
-          </Dialog>
-        </TableCell>
-        <TableCell className="font-mono text-xs font-medium">
-          490-1u4190481-41
-        </TableCell>
-        <TableCell className="text-muted-foreground">há 15 minutos</TableCell>
-        <TableCell></TableCell>
-        <TableCell className="font-medium">Yago Massaroni</TableCell>
-        <TableCell className="font-medium">R$140.00</TableCell>
-        <TableCell>
-          <Button variant="outline" size="xs">
-            <ArrowRight className="mr-2 h-3 w-3" />
-            Aprovar
-          </Button>
-        </TableCell>
-        <TableCell>
-          <Button variant="ghost" size="xs">
-            <X className="mr-2 h-3 w-3" />
-            Cancelar
-          </Button>
-        </TableCell>
-      </TableRow>
-    </>
+          <OrderDetails />
+        </Dialog>
+      </TableCell>
+      <TableCell className="font-mono text-xs font-medium">
+        {order.orderId}
+      </TableCell>
+      <TableCell className="text-muted-foreground">
+        {formatDistanceToNow(order.createdAt, {
+          locale: ptBR,
+          addSuffix: true
+        })}
+      </TableCell>
+      <TableCell>
+        <OrderStatus status={order.status} />
+      </TableCell>
+      <TableCell className="font-medium">{order.customerName}</TableCell>
+      <TableCell className="font-medium">{order.total.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      })}</TableCell>
+      <TableCell>
+        <Button variant="outline" size="xs">
+          <ArrowRight className="mr-2 h-3 w-3" />
+          Aprovar
+        </Button>
+      </TableCell>
+      <TableCell>
+        <Button variant="ghost" size="xs">
+          <X className="mr-2 h-3 w-3" />
+          Cancelar
+        </Button>
+      </TableCell>
+    </TableRow>
   );
 }
